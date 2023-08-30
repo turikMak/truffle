6d8c732fd202ef74ca59a18e110c56f214eb59dd
turikMak <137326144+turikMak@users.noreply.github.com>
2023-08-30 16:28:06 +0300

    Update geth-accounts.js

--- geth-accounts.js
+++ geth-accounts.js
@@ -2,3 +2,4 @@
  * @author  cpurta <cpurta@gmail.com>
  * @github https://github.com/cpurta/geth-devnet
  * This code comes from Christopher Purta's `geth-devnet` project.
+ * geth --dev seeds with a single account so we need to spin
