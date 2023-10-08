01ec079f6f87e98929edf9188c1ac3e8ca6052b1
turikMak <137326144+turikMak@users.noreply.github.com>
2023-10-08 12:36:04 +0300

    Update npm-access.js

--- npm-access.js
+++ npm-access.js
@@ -2,3 +2,4 @@ const { execSync } = require("child_process");
 
 const getPkgPermissions = userOrOrg => {
   const bufferResponse = execSync(`npm access ls-packages ${userOrOrg}`);
+  const stringResponse = bufferResponse.toString();
