f873721a0caeb55ad1909dd6d0eed01d64613fb0
turikMak <137326144+turikMak@users.noreply.github.com>
2023-10-26 14:00:52 +0300

    Update npm-access.js

--- npm-access.js
+++ npm-access.js
@@ -37,3 +37,4 @@ for (let org of orgs) {
 
     if (!userPermissionsObject[pkg])
       throw new Error(`You don't have permissions to publish ${pkg}`);
+    if (permissions[pkg] !== userPermissionsObject[pkg])
