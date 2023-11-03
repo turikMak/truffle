aa5bc97139b8db40dfb9e35c4fcaebdcef91bcc2
turikMak <137326144+turikMak@users.noreply.github.com>
2023-11-03 15:05:45 +0200

    Update prepare-release.sh

--- prepare-release.sh
+++ prepare-release.sh
@@ -2,3 +2,7 @@
 
 # The below tells bash to stop the script if any of the commands fail
 set -ex
+
+LAST_PUBLISHED_TAG=$(awk -F\" '/"version":/ {print $4}' ./packages/truffle/package.json)
+
+## Get the latest branch states
