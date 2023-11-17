2fa77b5ea271fb4a506a47cfaa558c3de257ba7c
turikMak <137326144+turikMak@users.noreply.github.com>
2023-11-17 04:50:39 +0200

    Update publish-dist-tag.sh

--- publish-dist-tag.sh
+++ publish-dist-tag.sh
@@ -8,3 +8,5 @@ distTag=$1 # first arg after yarn publish-dist-tag
 # Optional `lerna version` 'bump' command
 # must be an explicit version string _or_ one of:
 #   'major', 'minor', 'patch', 'premajor', 'preminor', 'prepatch', or 'prerelease'.
+# Omitting will prompt the user
+lernaVersionCommand=$2
